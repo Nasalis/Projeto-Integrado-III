caf5da4477e6807944c4299afc9ede9ed1791492
Davi dos Santos Freitas <davidossantosfreitas@hotmail.com>
2022-10-25 17:30:04 -0300

    feat: remove action bar

--- app/src/main/java/com/example/ramirez/MainActivity.java
+++ app/src/main/java/com/example/ramirez/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_in);
+        getSupportActionBar().hide();
     }
 }
