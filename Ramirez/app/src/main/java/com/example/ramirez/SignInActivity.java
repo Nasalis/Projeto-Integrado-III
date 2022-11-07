ddca63ce4ca9e9a7c141f68c7772e00190076a72
Davi dos Santos Freitas <davidossantosfreitas@hotmail.com>
2022-11-07 14:20:41 -0300

    refactor: update ids and make navigation

--- Ramirez/app/src/main/java/com/example/ramirez/SignInActivity.java
+++ Ramirez/app/src/main/java/com/example/ramirez/SignInActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+    private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +18,19 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sing_in);
 
         TextView goToSingUpActivity = findViewById(R.id.goToSingUpActivity);
+        this.loginBtn = findViewById(R.id.btnLogin);
+
         goToSingUpActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
+        });
+
+        this.loginBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
         });
     }
 }
