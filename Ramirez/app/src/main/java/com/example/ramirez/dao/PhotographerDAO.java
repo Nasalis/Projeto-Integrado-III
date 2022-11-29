edd94932a56a1d4a27504214386217f9e79daaea
Davi dos Santos Freitas <davidossantosfreitas@hotmail.com>
2022-11-29 12:40:07 -0300

    style: remove unused import

--- Ramirez/app/src/main/java/com/example/ramirez/dao/PhotographerDAO.java
+++ Ramirez/app/src/main/java/com/example/ramirez/dao/PhotographerDAO.java
@@ -1,9 +1,18 @@
 package com.example.ramirez.dao;
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
 import com.example.ramirez.model.Photographer;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PhotographerDAO implements IPhotographerDAO {
     private static Context context;
@@ -38,6 +47,30 @@ public class PhotographerDAO implements IPhotographerDAO {
         photographer.add(new Photographer("2", "Lucas Gomez", "SP", "Santos", "123", specializations2, new Float[]{40f, 100f}));
         photographer.add(new Photographer("3", "Matheus Gomez", "SP", "Santos", "123", specializations3, new Float[]{100f, 300f}));
 
+    }
+
+    public void getPhotographersFromDatabase() {
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://10.0.2.2:3001/users";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                if (response.isSuccessful()) {
+                    System.out.println("DEU CERTOOO AAAAAAAAAAA");
+                }
+            }
+        });
     }
 
     @Override
