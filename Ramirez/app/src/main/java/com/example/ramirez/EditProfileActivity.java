89e89854554b5b449b5516bda8ac35d1c002a9c4
Davi dos Santos Freitas <davidossantosfreitas@hotmail.com>
2022-12-03 16:59:05 -0300

    feat: add new informations to respective fields

--- Ramirez/app/src/main/java/com/example/ramirez/EditProfileActivity.java
+++ Ramirez/app/src/main/java/com/example/ramirez/EditProfileActivity.java
@@ -27,8 +27,21 @@ public class EditProfileActivity extends AppCompatActivity {
 
         Photographer currentPhotographer = usersService.getPhotographer(sessionManager.fetchUserId());
 
-        TextView userName = findViewById(R.id.editNameUserProfile);
+        TextView userName = findViewById(R.id.nameUserEdit);
+        TextView userEmail = findViewById(R.id.emailUserEdit);
+        TextView userBio = findViewById(R.id.bioEdit);
+        TextView userCity = findViewById(R.id.cityEdit);
+        TextView userState = findViewById(R.id.stateEdit);
+        TextView userMinValue = findViewById(R.id.minValueEdit);
+        TextView userMaxValue = findViewById(R.id.maxValueEdit);
 
         userName.setText(currentPhotographer.getName());
+        userEmail.setText(currentPhotographer.getName());
+        userBio.setText(currentPhotographer.getBio());
+        userCity.setText(currentPhotographer.getCity());
+        userState.setText(currentPhotographer.getState());
+        userMinValue.setText(Float.toString(currentPhotographer.getMinValue()));
+        userMaxValue.setText(Float.toString(currentPhotographer.getMinValue()));
+
     }
 }
